1ac535ac98fa34214e7956ddc4deb763f5e859cb
paolo975 <paolo.varano@gmail.com>
2013-01-16 15:57:59 +0100

    Update js/employeelist.js

--- js/employeelist.js
+++ js/employeelist.js
@@ -3,7 +3,10 @@ var dbCreated = false;
 
 var scroll = new iScroll('wrapper', { vScrollbar: false, hScrollbar:false, hScroll: false });
 
-document.addEventListener("deviceready", onDeviceReady, false);
+$(document).ready(function() {
+  // Handler for .ready() called.
+  document.addEventListener("deviceready", onDeviceReady, true);
+});
 
 function onDeviceReady() {
     db = window.openDatabase("EmployeeDirectoryDB", "1.0", "PhoneGap Demo", 200000);
